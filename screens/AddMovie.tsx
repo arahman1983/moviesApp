4fb197007563df92a5f9c651cc3e5810d5e9cc32
AbdelrahmanElhussiny <arahman1783@yahoo.com>
2021-11-28 09:09:47 +0200

    manage favorites movies

--- screens/AddMovie.tsx
+++ screens/AddMovie.tsx
@@ -7,9 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function AddMovie() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>ADD MOVIE</Text>
     </View>
   );
 }
@@ -18,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
